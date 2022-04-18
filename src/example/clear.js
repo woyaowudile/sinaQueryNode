8f54a1d0f9c5886245ec3a2c220a630d41b9e811
meng.tianxiang <meng.tianxiang@rongzer.com>
2022-04-18 10:49:43 +0800

    clear - all

--- src/example/clear.js
+++ src/example/clear.js
@@ -8,14 +8,24 @@ module.exports = function (app, connection) {
     app.get("/api/clear", async (req, res) => {
         console.log("-------------开始执行 /api/clear---------------");
         let { type = "models" } = req.query;
-        const keys = Object.keys(modelsCode);
-        const sql = `DROP TABLE ${keys.map((v) => `${SQL.base}_${v}`)}`;
+        let keys = Object.keys(modelsCode);
+        switch (type) {
+            case "all":
+                const res = await SQL.querySQL({
+                    connection,
+                    distinct: `DISTINCT type`,
+                    name: "ig502_list",
+                });
+                keys = keys.concat(res.data.map((v) => v.type));
+                break;
+        }
+        let sql = `DROP TABLE ${keys.map((v) => `${SQL.base}_${v}`)}`;
         connection.query(sql, (err, res) => {
             if (err) {
-                console.log(`>> clear失败: ${err.message}`);
+                console.log(`>> clear_${type}失败: ${err.message}`);
                 return;
             }
-            console.log("-------------执行完成 /api/clear---------------");
+            console.log(`》》-- 执行完成 /api/clear：${type} --《《`);
             request(
                 {
                     url: "http://localhost:3334/api/createNewTables",
