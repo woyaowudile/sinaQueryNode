7f1f5418e4d3532cb58ac2c6f1e08ced33bad6bb
meng.tianxiang <meng.tianxiang@rongzer.com>
2022-07-27 15:21:33 +0800

    update

--- src/example/test.js
+++ src/example/test.js
@@ -1,12 +1,12 @@
 /** @format */
 
-const API = require("../api");
+const SQL = require("../sql");
 
 module.exports = function (app, connection) {
     app.get("/api/test", async (req, res) => {
         console.log("-------------开始执行 /api/test---------------");
-        const res = await API.getList({ connection });
+        const result = await SQL.getList({ connection });
         console.log(`》》-- 执行完成 /api/test --《《`);
-        res.send("ok");
+        res.send({ data: result, message: "成功", code: 0 });
     });
 };
